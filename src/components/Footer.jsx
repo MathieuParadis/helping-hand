8fd8d8d1230c6f8895d51595873adaa4d7ae0041
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-13 08:54:22 +0800

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,13 @@ const Footer = () => {
     <div className="footer">
       <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center">
         <img src={logo} alt="Boulder Bike Tour logo" className="logo mb-4 mb-lg-0" />
-        <div className="col-7">
-          <h4 className="mb-2 mb-lg-3"><strong>About us</strong></h4>
-          <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="align-self-lg-start col-7 col-xl-8 mb-4 mb-lg-0">
+          <h4 className="mb-3 text-center text-lg-start"><strong>About us</strong></h4>
+          <p className="mb-0 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <div>
-          <div className="mb-4 mb-lg-0 text-center text-lg-start">
+        <div className="align-self-lg-start">
+          <h4 className="mb-3 text-center text-lg-start"><strong>Follow us</strong></h4>
+          <div className="mb-3 text-center text-lg-start">
             <a href="https://twitter.com/" target="_blank">
               <FontAwesomeIcon icon="fa-brands fa-twitter-square" size="3x" className="navlink"/>
             </a>
