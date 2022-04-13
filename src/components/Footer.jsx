89349c7327d51718c78ba407b4ccccb978bd9c48
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-13 09:56:12 +0800

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,12 @@ const Footer = () => {
         <img src={logo} alt="Boulder Bike Tour logo" className="logo mb-4 mb-lg-0" />
         <div className="align-self-lg-start col-7 col-xl-8 mb-4 mb-lg-0">
           <h4 className="mb-3 text-center text-lg-start"><strong>About us</strong></h4>
-          <p className="mb-0 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p className="mb-0 text-justify">
+            Helping Hand is a local charity whose mission is to help to build a strong community by promoting the good.
+            We aim to educate, and inspire people to embrace kindness and charitable giving, and in doing so, be the change they want to be in the world.
+            To achieved this goal, we have developed a plateform to help connecting people in need with volunteers within the community.
+            We believe that by making it easier for people to give and reach out to help those less fortunate than themselves, they will feel empowered and are more likely to volunteer. 
+          </p>
         </div>
         <div className="align-self-lg-start">
           <h4 className="mb-3 text-center text-lg-start"><strong>Follow us</strong></h4>
@@ -31,13 +36,13 @@ const Footer = () => {
           </div>
           <div className="mb-4 mb-lg-0 text-center text-lg-start">
             <a href="https://twitter.com/" target="_blank">
-              <FontAwesomeIcon icon="fa-brands fa-twitter-square" size="3x" className="navlink"/>
+              <FontAwesomeIcon icon="fa-brands fa-linkedin" size="3x" className="navlink"/>
             </a>
             <a href="https://facebook.com/" target="_blank">
-              <FontAwesomeIcon icon="fa-brands fa-facebook-square" size="3x" className="navlink mx-3"/>
+              <FontAwesomeIcon icon="fa-brands fa-pinterest" size="3x" className="navlink mx-3"/>
             </a>
             <a href="https://instagram.com/" target="_blank">
-              <FontAwesomeIcon icon="fa-brands fa-instagram" size="3x" className="navlink"/>
+              <FontAwesomeIcon icon="fa-brands fa-youtube-square" size="3x" className="navlink"/>
             </a>
           </div>
         </div>
