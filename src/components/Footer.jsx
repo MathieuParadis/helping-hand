1f7634174a10b4669b8c3df9650d252d0b522cc6
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-13 08:18:36 +0800

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,13 +12,12 @@ const Footer = () => {
     <div className="footer">
       <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center">
         <img src={logo} alt="Boulder Bike Tour logo" className="logo mb-4 mb-lg-0" />
-        <div>
-          <h4>About us</h4>
+        <div className="col-7">
+          <h4 className="mb-2 mb-lg-3"><strong>About us</strong></h4>
           <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div>
           <div className="mb-4 mb-lg-0 text-center text-lg-start">
-            <h5 className="mb-2 mb-lg-3"><strong>Follow us</strong></h5>
             <a href="https://twitter.com/" target="_blank">
               <FontAwesomeIcon icon="fa-brands fa-twitter-square" size="3x" className="navlink"/>
             </a>
@@ -30,7 +29,6 @@ const Footer = () => {
             </a>
           </div>
           <div className="mb-4 mb-lg-0 text-center text-lg-start">
-            <h5 className="mb-2 mb-lg-3"><strong>Follow us</strong></h5>
             <a href="https://twitter.com/" target="_blank">
               <FontAwesomeIcon icon="fa-brands fa-twitter-square" size="3x" className="navlink"/>
             </a>
