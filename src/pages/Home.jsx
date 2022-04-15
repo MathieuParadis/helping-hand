e6315e0595d268e97eb1519221ddf2affb19f32c
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-15 15:16:48 +0800

    Edited homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 // CONFIG IMPORTS
-import React from 'react';
+import React, {useEffect} from 'react';
+import { NavLink } from 'react-router-dom';
+
+// COMPONENTS IMPORTS
+
+// ASSETS IMPORTS
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="home">
-      Homepage
+      <div className="hero-section">
+        <div className="bg-picture"></div>
+        <div className="hero-content">
+        </div>
+      </div>
+
+      <div className="rules-section">
+        <div className="colored-bg">
+        </div>
+      </div>
+
+      <div className="testimonials-section">
+      </div>
+
     </div>
   );
 };
