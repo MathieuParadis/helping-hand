a4a920dd51030322c011ba8dc90a2486007b1235
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-19 05:46:56 +0800

    edited  mapRequests page

--- src/pages/MapRequests.jsx
+++ src/pages/MapRequests.jsx
@@ -9,24 +9,30 @@ import { maptiler } from 'pigeon-maps/providers';
 import ReactLoading from 'react-loading';
 
 // DATA IMPORTS
-import riders from '../data/Riders';
+import requests from '../data/Requests';
 
 const maptilerProvider = maptiler('IwympTEN2FYbP2g5qdck', 'streets')
 
 const MapRequests = () => {
   const [center, setCenter] = useState([40.014984, -105.270546]); // default center: Boulder, Colorado
   const [zoom, setZoom] = useState(12);
-  const [currentRider, setCurrentRider] = useState("");
+  const [currentrequest, setCurrentrequest] = useState("");
   const [coordinatesBlueMarker, setCoordinatesBlueMarker] = useState([0, 0]);
   const [widthBlueMarker, setWidthBlueMarker] = useState(0);
+
+
+  const colorMaterial = "#F4A896";
+  const colorService = "#262F53"
+
+  const colorChoice = (request) => {
+    return request.request.type === "material" ? colorMaterial : colorService;
+  }
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-
-
     <div className="map-requests">
       <div class="d-flex flex-column justify-content-center align-items-center mx-0 my-3 py-3">
         <h1 className="text-primary text-center fw-bold pb-3 pb-md-4">Requests around me</h1>
@@ -34,14 +40,14 @@ const MapRequests = () => {
 
 
           <div className="map d-flex justify-content-center align-items-center my-4">
-            { riders && riders.length > 0 ? 
+            { requests && requests.length > 0 ? 
               (
                 <Map provider={maptilerProvider} dprs={[1, 2]} center={center} defaultZoom={12} zoom={zoom}>
                   <ZoomControl />
                   {
-                    riders.map((rider) => {
+                    requests.map((request) => {
                       return (
-                        <Marker width={100} anchor={[rider.position.lat, rider.position.lgn]} color={'#ffd700'} key={rider.first_name + rider.last_name} />
+                        <Marker width={100} anchor={[request.request.position.lat, request.request.position.lgn]} color={colorChoice(request)} key={request.requester.first_name + request.requester.last_name} />
                       )
                     })
                   }
