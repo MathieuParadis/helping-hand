97b76ac9b1d1888d5b0c128b51dd092c70b6c6d9
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-18 09:40:24 +0800

    edited hero section in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // CONFIG IMPORTS
 import React, {useEffect} from 'react';
+import { NavLink } from 'react-router-dom';
 
 // COMPONENTS IMPORTS
 import TestimonyCard from '../components/TestimonyCard';
@@ -18,7 +19,13 @@ const Home = () => {
     <div className="home">
       <div className="hero-section">
         <div className="bg-picture"></div>
-        <div className="hero-content">
+        <div className="hero-content d-flex flex-column justify-content-center align-items-center justify-content-md-start align-items-md-start">
+          <h1 className="hero-title text-white fw-bold">Make a difference right<br></br>outside your door</h1>
+          <p className="hero-sub-title text-white my-2">Help someone in need</p>
+          <div className="d-flex flex-column flex-md-row justify-content-center align-items-center justify-content-md-start mt-4 pt-4">
+            <NavLink exact="true" to="/signup" className="btn button-w250 button-primary hero-button mb-3 mb-md-0 me-md-3">Join today</NavLink>
+            <NavLink exact="true" to="/how-it-works" className="btn button-w250 button-secondary hero-button ms-md-3">Learn more</NavLink>
+          </div>
         </div>
       </div>
 
