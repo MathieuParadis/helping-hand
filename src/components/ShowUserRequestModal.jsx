23966ad32b2f50366e9fd30fe52454bf21e78f3c
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-20 16:14:02 +0800

    created ShowUserRequestModal

---
+++ src/components/ShowUserRequestModal.jsx
@@ -0,0 +1,28 @@
+// CONFIG IMPORTS
+import React from 'react';
+
+const ShowUserRequestModal = ({request}) => {
+  const closeShowUserRequestModal = () => {
+    const modifyCredentialsModal = document.querySelector(".show-user-request-modal");
+    modifyCredentialsModal.style.visibility = "hidden";
+    document.querySelector("body").classList.remove("clicked");
+  }
+
+  return (
+    <div className="show-user-request-modal">
+      <div className="show-user-request-modal-overlay"></div>
+      <div className="show-user-request-modal-white-bg">
+        <div className="show-user-request-modal-content d-flex flex-column justify-content-between p-3 p-sm-4 p-md-5">
+          <div>
+            Show user request modal
+            {request.request.title}
+          </div>
+          <button className="btn button-danger button-chat p-1" onClick={() => closeShowUserRequestModal()}>Cancel</button>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowUserRequestModal;
--- src/pages/UserRequests.jsx
+++ src/pages/UserRequests.jsx
@@ -4,6 +4,7 @@ import React, {useEffect} from 'react';
 // COMPONENTS IMPORTS
 import MaterialRequestCard from '../components/MaterialRequestCard';
 import ServiceRequestCard from '../components/ServiceRequestCard';
+import ShowUserRequestModal from '../components/ShowUserRequestModal';
 
 // DATA IMPORTS
 import requests from '../data/Requests';
@@ -18,31 +19,34 @@ const UserRequests = () => {
   }, []);
 
   return (
-    <div className="user-requests">
-      <div class="container d-flex justify-content-center w-100">
-        <div class="d-flex flex-column align-items-center my-3 py-3 w-100">
-          <h1 className="text-primary text-center fw-bold pb-3 pb-md-4">My Requests</h1>
-          <button className="btn button-w150 button-outline-primary" id="signup-btn" onClick={(e) => openNewRequestModal()}>Request help</button>
-          <div className="caption d-flex flex-column flex-md-row align-self-start my-4">
-            <div className="d-flex align-items-center my-2 pe-md-5">
-              <span className="me-2" id="material"></span><p className="h5 m-0">Material need</p>
+    <>
+      {/* <ShowUserRequestModal request /> */}
+      <div className="user-requests">
+        <div class="container d-flex justify-content-center w-100">
+          <div class="d-flex flex-column align-items-center my-3 py-3 w-100">
+            <h1 className="text-primary text-center fw-bold pb-3 pb-md-4">My Requests</h1>
+            <button className="btn button-w150 button-outline-primary" id="signup-btn" onClick={(e) => openNewRequestModal()}>Request help</button>
+            <div className="caption d-flex flex-column flex-md-row align-self-start my-4">
+              <div className="d-flex align-items-center my-2 pe-md-5">
+                <span className="me-2" id="material"></span><p className="h5 m-0">Material need</p>
+              </div>
+              <div className="d-flex align-items-center my-2">
+                <span className="me-2" id="service"></span><p className="h5 m-0">Punctual service</p>
+              </div>
             </div>
-            <div className="d-flex align-items-center my-2">
-              <span className="me-2" id="service"></span><p className="h5 m-0">Punctual service</p>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 d-flex justify-content-center w-100">
+              { 
+                requests.map((request) => {
+                  return (
+                    request.request.type === "material" ? <MaterialRequestCard request={request} /> : <ServiceRequestCard request={request} />
+                  )
+                })
+              }
             </div>
-          </div>
-          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 d-flex justify-content-center w-100">
-            { 
-              requests.map((request) => {
-                return (
-                  request.request.type === "material" ? <MaterialRequestCard request={request} /> : <ServiceRequestCard request={request} />
-                )
-              })
-            }
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
