d707bb20bb69b427d6a926fe53b1ca246aba8dbd
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-20 16:26:19 +0800

    created NewUserRequestModal

--- src/components/NewUserRequestModal.jsx
+++ src/components/NewUserRequestModal.jsx
@@ -1,7 +1,7 @@
 // CONFIG IMPORTS
 import React from 'react';
 
-const NewUserRequestModal = ({request}) => {
+const NewUserRequestModal = () => {
   const closeNewUserRequestModal = () => {
     const newUserRequestModal = document.querySelector(".new-user-request-modal");
     newUserRequestModal.style.visibility = "hidden";
@@ -13,12 +13,10 @@ const NewUserRequestModal = ({request}) => {
       <div className="new-user-request-modal-overlay"></div>
       <div className="new-user-request-modal-white-bg">
         <div className="new-user-request-modal-content d-flex flex-column justify-content-between p-3 p-sm-4 p-md-5">
-          {/* <div>
-            Show user request modal
-            {request.request.title}
+          <div>
+            New user request modal
           </div>
-          <button className="btn button-danger button-chat p-1" onClick={() => closeNewUserRequestModal()}>Cancel</button> */}
-
+          <button className="btn button-danger button-chat p-1" onClick={() => closeNewUserRequestModal()}>Cancel</button>
         </div>
       </div>
     </div>
