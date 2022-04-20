4dcf5939b77311032d42bc536ff2483a601e4383
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-20 11:40:41 +0800

    edited ShowRequestModal

--- src/components/ShowRequestModal.jsx
+++ src/components/ShowRequestModal.jsx
@@ -3,8 +3,6 @@ import React, {useState, useEffect} from 'react';
 
 const ShowRequestModal = ({request}) => {
   const modal = document.querySelector(".show-request-modal");
-  const modalOverlay = document.querySelector(".modal-overlay");
-  const modalContent = document.querySelector(".modal-content");
 
   const closeModal = () => {
     modal.style.visibility = "hidden";
@@ -17,14 +15,26 @@ const ShowRequestModal = ({request}) => {
     closeModal();
   };
 
+  const openChat = () => {
+    alert("opening chat");
+  }
+
   return (
     <div className="show-request-modal">
       <div className="modal-overlay"></div>
-      <div className="modal-content p-3">
+      <div className="modal-white-bg">
         {request && (
-          <>
-          {request.request.title}
-          </>
+          <div className="modal-content d-flex flex-column justify-content-between p-3 p-sm-4 p-md-5">
+            <div>
+              <h2 className="text-primary fw-bold mb-5">{request.request.title}</h2>
+              <p className="mb-4"><strong>Requester: </strong>{request.requester.first_name} {request.requester.last_name}</p>
+              <p className="mb-4"><strong>Type: </strong>{request.request.type}</p>
+              <p className="mb-4"><strong>Location: </strong>{request.request.location}</p>
+              <p className="mb-4"><strong>Description: </strong>{request.request.description}</p>
+            </div>
+            <button className="btn button-primary button-chat p-1" onClick={() => openChat()} >Volunteer</button>
+
+          </div>
         )}
       </div>
     </div>
