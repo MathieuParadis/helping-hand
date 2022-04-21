6b7e6d091995673e09209ec32ca215e1866b546f
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-21 09:36:00 +0800

    edited dropdownMenuOptionsRequestCard

--- src/components/DropdownMenuOptionsRequestCard.jsx
+++ src/components/DropdownMenuOptionsRequestCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const DropdownMenuOptionsRequestCard = ({request, setOpenEditModal}) => {
+  const markAsFulfilled = () => {
+  alert("marking request as fulfilled");
+}
+
+  const deleteRequest = () => {
+    window.confirm("You are about to delete this request. \n\nAre you sure?");
+  }
   return (
     <div className="dropdown-menu-options-request-card ms-2">
       <ul className = "list-unstyled m-0 p-0">
-        <li className="m-0 p-2 pointer">Mark as fulfilled</li>
+        <li className="m-0 p-2 pointer" onClick={() => markAsFulfilled()}>Mark as fulfilled</li>
         <li className="m-0 p-2 pointer" onClick={() => setOpenEditModal(request)}>Edit request</li>
         <hr className="m-0"></hr>
-        <li className="m-0 p-2 pointer">Delete request</li>
+        <li className="m-0 p-2 pointer" onClick={() => deleteRequest()}>Delete request</li>
       </ul>
 
     </div>
