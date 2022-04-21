6a34a499d2ebc3f55f4d289a7e21667cc8722b74
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-21 10:48:20 +0800

    edited MaterialRequestCard

--- src/components/MaterialRequestCard.jsx
+++ src/components/MaterialRequestCard.jsx
@@ -5,24 +5,15 @@ import React from 'react';
 import DropdownMenuOptionsRequestCard from '../components/DropdownMenuOptionsRequestCard';
 
 const MaterialRequestCard = ({request, setOpenShowModal, setOpenEditModal}) => {
-  const openMenuRequestCard = () => {
+  const toggleMenuRequestCard = () => {
     let menuRequestCard = document.querySelector("#" + request.requester.last_name + "> div.dropdown-menu-options-request-card");
-    menuRequestCard.style.visibility = 'visible';
-    alert(menuRequestCard.textContent);
-  }
 
-  const closeMenuRequestCard  = () => {
-    let menuRequestCard = document.querySelector("#" + request.requester.last_name + "> div.dropdown-menu-options-request-card");
-    
     if (menuRequestCard.style.visibility === 'visible') {
       menuRequestCard.style.visibility = 'hidden';
+    } else {
+      menuRequestCard.style.visibility = 'visible';
     }
   }
-
-  window.onclick = (event) => {
-    event.target !== document.querySelector("#" + request.requester.last_name + "> div.dropdown-menu-options-request-card") &&
-    closeMenuRequestCard();
-  };
 
   return (
     <div className="request-card">
@@ -39,7 +30,7 @@ const MaterialRequestCard = ({request, setOpenShowModal, setOpenEditModal}) => {
           </div>
           <div className="dropdown" id={request.requester.last_name}>
             <DropdownMenuOptionsRequestCard request={request} setOpenEditModal={setOpenEditModal} />
-            <button className="btn button-outline-primary button-dropdown w-100 h-100 p-1" onClick={(e) => openMenuRequestCard()}>
+            <button className="btn button-outline-primary button-dropdown w-100 h-100 p-1" onClick={(e) => toggleMenuRequestCard()}>
               More options
             </button>
           </div>
