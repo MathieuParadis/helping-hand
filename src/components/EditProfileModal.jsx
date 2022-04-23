e9e09d2f0b276ae5948de8bc35478b9c7dc90697
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-23 12:09:07 +0800

    edited EditProfileModal

--- src/components/EditProfileModal.jsx
+++ src/components/EditProfileModal.jsx
@@ -46,27 +46,29 @@ const EditProfileModal = ({userData}) => {
     <div className="edit-profile-modal">
       <div className="edit-profile-modal-overlay"></div>
       <div className="edit-profile-modal-white-bg">
-        <div className="edit-profile-modal-content d-flex flex-column justify-content-between align-items-center w-100 p-4 p-md-5">
+        <div className="edit-profile-modal-content d-flex flex-column align-items-center w-100 p-4 p-md-5">
           <h2 className="text-primary fw-bold mb-5">Edit my information</h2>
-          <div className="form-container w-100">
-            <form onSubmit={updateInformationRequest} className="d-flex flex-column justify-content-center w-100">
-              <div className="input mb-3">
-                <label htmlFor="first_name" className="mb-1">First name</label>
-                <input type="text" className="form-control" id="first-name" aria-describedby="first_name input field" placeholder="First name" value={fname} onChange={(e) => setFname(e.target.value)} required />
-                <img src={profile_icon} alt="profile_icon" className="profile-icon" />
-              </div>
-              <div className="input mb-3">
-                <label htmlFor="last_name" className="mb-1">Last name</label>
-                <input type="text" className="form-control" id="last-name" aria-describedby="last_name input field" placeholder="Last name" value={lname} onChange={(e) => setLname(e.target.value)} required />
-                <img src={profile_icon} alt="profile_icon" className="profile-icon" />
-              </div>
-              <div className="file-input mb-3">
-                <label htmlFor="ID" className="mb-1">ID&nbsp;<small className="caption">(jpeg, png only)</small></label>
-                <div className="d-flex align-items-center">
-                  <img src={plus_icon} alt="plus_icon" className="plus-icon pointer" onClick={handleClick} />
-                  <p className="m-0 ps-3" id="labelHiddenFileInput">{getFileName(id)}</p>
+          <div className="form-container d-flex flex-grow-1 w-100">
+            <form onSubmit={updateInformationRequest} className="d-flex flex-column justify-content-between w-100">
+              <div>
+                <div className="input mb-3">
+                  <label htmlFor="first_name" className="mb-1">First name</label>
+                  <input type="text" className="form-control" id="first-name" aria-describedby="first_name input field" placeholder="First name" value={fname} onChange={(e) => setFname(e.target.value)} required />
+                  <img src={profile_icon} alt="profile_icon" className="profile-icon" />
                 </div>
-                <input type="file" className="" id="hiddenFileInput" aria-describedby="file input field" onInput={(e) => setId_card(e.target.value)} onChange={(e) => handleChange()} accept="image/png, image/jpeg" ref={hiddenFileInput} />
+                <div className="input mb-3">
+                  <label htmlFor="last_name" className="mb-1">Last name</label>
+                  <input type="text" className="form-control" id="last-name" aria-describedby="last_name input field" placeholder="Last name" value={lname} onChange={(e) => setLname(e.target.value)} required />
+                  <img src={profile_icon} alt="profile_icon" className="profile-icon" />
+                </div>
+                <div className="file-input mb-3">
+                  <label htmlFor="ID" className="mb-1">ID&nbsp;<small className="caption">(jpeg, png only)</small></label>
+                  <div className="d-flex align-items-center">
+                    <img src={plus_icon} alt="plus_icon" className="plus-icon pointer" onClick={handleClick} />
+                    <p className="m-0 ps-3" id="labelHiddenFileInput">{getFileName(id)}</p>
+                  </div>
+                  <input type="file" className="" id="hiddenFileInput" aria-describedby="file input field" onInput={(e) => setId_card(e.target.value)} onChange={(e) => handleChange()} accept="image/png, image/jpeg" ref={hiddenFileInput} />
+                </div>
               </div>
               <div className="d-flex flex-column flex-md-row justify-content-md-center mt-4">
                 <button type="submit" className="btn button-success button-modal me-0 me-md-2 mb-3 mb-md-0 p-1">Update profile</button>
