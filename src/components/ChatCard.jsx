fe255c0f710b16167e2a2ecbbec639f837a5f64e
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-25 18:39:18 +0800

    edited chatCard component

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ChatCard = ({chat}) => {
   return (
-    <div className="chat-card w-100 border-bottom-grey p-3 d-flex flex-column justify-content-between">
+    <div className="chat-card w-100 border-bottom-grey p-3 d-flex flex-column justify-content-between pointer">
       <h5>{chat.request.title}</h5>
       <p className="m-0">by {chat.request.requester.first_name} {chat.request.requester.last_name}</p>
     </div>
