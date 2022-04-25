fb30213313eff3b4cc2bfa4d61c74d0a547bb15e
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-25 10:51:39 +0800

    edited rules data

--- src/data/Rules.jsx
+++ src/data/Rules.jsx
@@ -30,21 +30,27 @@ const rules = {
     },
     {
       title: "Be proactive and participate",
-      description: "Receiving help is an active job. Be proactive, participate and help the volunteer(s) in every way possible",
+      description: "Receiving help is an active job. Be proactive, participate and help the volunteer(s) in every way possible.",
       img: be_proactive_illustration,
     },
     {
       title: "Report abuse",
-      description: "To keep our platform and users safe, we have set up a strong policy against misbehaviour and abuses. Spammers will be reported and banned",
+      description: "To keep our platform and users safe, we have set up a strong policy against misbehaviour and abuses. Spammers will be reported and banned.",
       img: report_abuse_illustration,
     },
   ],
   rulespage: [
     {
-      description: "Subscribing to the Helping Hand app, as well as using our service is completely free. Volunteers cannot ask for money or donations.",
+      description: "Helping Hand is a local charity that does not aim to make money. Subscribing to our app as well as using our service is and will remain free",
     },
     {
-      description: "If you want to give back to the community but are unable to do so, we encourage you to donate to your local associations or charitities such as the Red Cross.",
+      description: "Volunteers cannot ask for money or donations. Offering your time to help others means, you shares core values with us, such as generosity, love and respect, that are beyond money.",
+    },
+    {
+      description: "If you want to help but are concerned of not being able to help, do not worry. There is always a way. Contact the person in need and engage the conversation to figure out a solution.",
+    },
+    {
+      description: "If you want to give back to the community, but are unable to do so, we encourage you to donate to your local associations or charitities such as the Red Cross.",
     },
     {
       description: "To ensure the safety of all our users, we verify the identity of all our applicants.",
@@ -53,24 +59,16 @@ const rules = {
       description: "While asking for help, well defined your need in advance. Anyone volunteering to help you has to understand exactly what the request is about.",
     },
     {
-      description: "Receiving help is an active job. Be proactive, participate and help the volunteer(s) in every way possible",
+      description: "Receiving help is an active job. Be proactive, participate and help the volunteer(s) in every way possible.",
     },
     {
-      description: "To keep our platform and users safe, we have set up a strong policy against misbehaviour and abuses. Spammers will be reported and banned",
+      description: "Remember, be patient! We are all human beings. All volunteers genuinely want to help but not everyone contacting you might be able to do so.",
+    },
+    {
+      description: "The safety of our users is our top priority. Aggresive and dangerous behaviors are not tolerated. Offenders will be reported and banned.",
     },
   ],
 
 };
 
 export default rules;
-
-
-          {/* <ul>
-            <li className="mb-2">Can't ask for money or donations</li>
-            <li className="mb-2">All help is free. Nobody gets paid, ever</li>
-            <li className="mb-2">The "ask" has to be well defined in advance. Anyone signing up to help you has to know exactly what they are getting into</li>
-            <li className="mb-2">Spammers or salesmen abusing the platform get banned</li>
-            <li className="mb-2">Reviewing how the encounter went is mandatory for everyone involved</li>
-            <li className="mb-2">People's identities should be verified to ensure accountability and prevent abuse</li>
-            <li className="mb-2">The one getting the help has to participate and help the helpers in every way they can</li>
-          </ul> */}
