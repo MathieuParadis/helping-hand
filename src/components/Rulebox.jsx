b53032bca237f0cd57ed843d78b9182b7fb1c1d3
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-04-25 09:16:35 +0800

    edited ruleBox component

--- src/components/Rulebox.jsx
+++ src/components/Rulebox.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const RuleBox = ({rule}) => {
   const {description} = rule
   return (
-    <div className="rule-box">
-      <p>{description}</p>
+    <div className="rule-box d-flex align-items-center p-3 mb-5">
+      <p className="m-0">{description}</p>
     </div>
   );
 };
