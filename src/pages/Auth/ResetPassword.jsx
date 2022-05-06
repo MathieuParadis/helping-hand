1631bd1038756cbeeb4d6b489e096e632188875b
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-06 16:17:11 +0800

    edited resetPassword page

--- src/pages/Auth/ResetPassword.jsx
+++ src/pages/Auth/ResetPassword.jsx
@@ -1,5 +1,9 @@
 // CONFIG IMPORTS
-import React, {useEffect} from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useParams } from 'react-router';
+
+// CONTEXT IMPORTS
+import FlashContext from '../../components/Context/FlashContext';
 
 // ASSETS IMPORTS
 import reset_password_illustration from '../../assets/images/reset_password_illustration.svg';
@@ -8,8 +12,61 @@ import mail_icon from '../../assets/logos/mail_logo.svg';
 import lock_icon from '../../assets/logos/lock_logo.svg';
 
 const ResetPassword = () => {
-  const postResetPasswordRequest = (e) => {
-    alert("reset password request");
+  const { setFlash } = useContext(FlashContext);
+  const { tokenSlug } = useParams();
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    const email_add = document.querySelector('#email').value;
+    const pw = document.querySelector('#password').value;
+
+    const data = {
+      email: email_add,
+      password: pw,
+      token: tokenSlug,
+    };
+
+    const url = 'http://localhost:3000/reset-password';
+
+    fetch(url, {
+      method: "POST",
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response.message ) {
+        setFlash({
+          type: 'success',
+          message: response.message,
+          display: true,
+        });
+        emptyFormFields();
+      } else {
+        setFlash({
+          type: 'danger',
+          message: response.error,
+          display: true,
+        })
+      }
+    })
+    .catch(error =>{
+      setFlash({
+        type: 'danger',
+        message: error,
+        display: true,
+      })
+    })
+  }
+
+  const emptyFormFields = () => {
+    document.querySelector('#email').value = "";
+    document.querySelector('#password').value = "";
+
   }
 
   useEffect(() => {
@@ -27,7 +84,7 @@ const ResetPassword = () => {
             <img src={auth_logo} alt="authentification logo" className="auth-logo align-self-center mb-2" />
             <h2 className="text-center pb-md-4 mb-5">Reset your<br></br>password</h2>
             <div className="form-container">
-              <form onSubmit={postResetPasswordRequest} className="d-flex flex-column justify-content-center">
+              <form onSubmit={resetPassword} className="d-flex flex-column justify-content-center">
                 <div className="input mb-3">
                   <label htmlFor="email" className="mb-1">Email</label>
                   <input type="email" className="form-control" id="email" aria-describedby="email input field" placeholder="Your email" required />
