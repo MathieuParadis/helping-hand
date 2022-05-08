dbb32e30612b34d5a6d9ffe4b9f7002849093857
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-09 07:41:02 +0800

    edited NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,9 +7,9 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <div className="container d-flex flex-column justify-content-center align-items-center">
-        <p className="title">Oops!</p>
-        <p className="h3">We can't seem to find the page you're looking for</p>
-        <p className="">Error code: 404</p>
+        <p className="title fw-bold text-primary">Oops!</p>
+        <p className="h3 text-center">We can't seem to find the page you're looking for.</p>
+        <p className="pt-2">Error code: 404</p>
         <NavLink exact="true" to="/" className="btn button-outline-primary border-radius-50 h4 mb-0">Back to homepage</NavLink>
       </div>
     </div>
