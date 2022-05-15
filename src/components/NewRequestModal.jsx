15591bdf2ba342aa8ccdd35d207499250ba6a656
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-15 14:15:04 +0800

    edited new request modal: connected to backend

--- src/components/NewRequestModal.jsx
+++ src/components/NewRequestModal.jsx
@@ -1,21 +1,104 @@
 // CONFIG IMPORTS
-import React, { useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+
+// CONTEXT IMPORTS
+import FlashContext from './Context/FlashContext';
+import UserContext from './Context/UserContext';
+
+// DATA IMPORTS
+import baseURL from '../data/BaseURL';
 
 const NewRequestModal = () => {
+  const { setFlash } = useContext(FlashContext);
+  const { user, setUser } = useContext(UserContext);
+
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [location, setLocation] = useState("");  
   const [lat, setLat] = useState("");
   const [long, setLong] = useState("");
+  const [description, setDescription] = useState("");
 
   const closeNewRequestModal = () => {
     const newRequestModal = document.querySelector(".new-request-modal");
     newRequestModal.style.visibility = "hidden";
     document.querySelector("body").classList.remove("clicked");
-    setLat("");
-    setLong("");
+    emptyFormFields();
     scrollTopComponent();
   }
 
-  const createRequest = () => {
-    alert("creating new request");
+  const createRequest = (e) => {
+    e.preventDefault();
+
+    const data = {
+      title: title,
+      request_type: type,
+      location: location,
+      lat: lat,
+      lng: long,
+      description: description
+    };
+
+    const url = `${baseURL}/requests`;
+    const token = localStorage.getItem('jwt_token');
+
+    fetch(url, {
+      method: "POST",
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(response => {
+      if (response.message) {
+        setFlash({
+          type: 'success',
+          message: response.message,
+          display: true,
+        })
+      } else {
+        const errors = response.error;
+        console.log(errors);
+        const arrayErrors = [];
+        Object.keys(errors).map(function(key, index) {
+          arrayErrors.push(`${key} ${errors[key][0]}`)
+          return true
+        })
+        const errorMessage = arrayErrors.join(" | ");
+        setFlash({
+          type: 'danger',
+          message: errorMessage,
+          display: true,
+        })
+      }
+      console.log(response)
+      closeNewRequestModal();
+    })
+    .catch(errors => {
+      closeNewRequestModal();
+      console.log(errors)
+      setFlash({
+        type: 'danger',
+        message: "An error occured, please try again",
+        display: true,
+      })
+    })
+  }
+
+  const emptyFormFields = () => {
+    setTitle("");
+    setType("");
+    setLocation("");
+    setLat("");
+    setLong("");
+    setDescription("");
   }
 
   const getPosition = () => {
@@ -34,6 +117,10 @@ const NewRequestModal = () => {
     modalTitle.scrollIntoView({ behavior: 'smooth' });
   }
 
+  useEffect(() => {
+    scrollTopComponent();
+  }, []);
+
   return (
     <div className="new-request-modal">
       <div className="new-request-modal-overlay"></div>
@@ -45,11 +132,11 @@ const NewRequestModal = () => {
               <div>
                 <div className="input mb-3">
                   <label htmlFor="title" className="mb-1">Title&nbsp;<small className="caption">(50 characters max)</small></label>
-                  <input type="text" className="form-control" id="title" aria-describedby="title input field" placeholder="Title" minLength="2" maxLength="50" required />
+                  <input type="text" className="form-control" id="title" aria-describedby="title input field" placeholder="Title" minLength="2" maxLength="50" value={title} onChange={(e) => setTitle(e.target.value)} required />
                 </div>
                 <div className="input my-3 d-flex flex-column">
                   <label htmlFor="type" className="mb-1">Type</label>
-                  <select className="select-form-control">
+                  <select className="select-form-control" value={type} onChange={(e) => setType(e.target.value)}>
                     <option value="" className="text-primary" disabled="disabled">-- Please choose a type --</option>
                     <option value='material'>Material</option>
                     <option value='service'>Service</option>
@@ -57,7 +144,7 @@ const NewRequestModal = () => {
                 </div>
                 <div className="input my-3">
                   <label htmlFor="location" className="mb-1">Location</label>
-                  <input type="text" className="form-control" id="location" aria-describedby="Location input field" placeholder="Location" minLength="2" maxLength="50" required />
+                  <input type="text" className="form-control" id="location" aria-describedby="Location input field" placeholder="Location" minLength="2" maxLength="50" value={location} onChange={(e) => setLocation(e.target.value)} required />
                 </div>
                 <div className="geo-position-section d-flex flex-column my-4">
                   <h5 className="mb-2">Geographical coordinates</h5>
@@ -75,11 +162,11 @@ const NewRequestModal = () => {
                 </div>
                 <div className="input my-4">
                   <label htmlFor="description" className="mb-1">Description&nbsp;<small className="caption">(300 characters max)</small></label>
-                  <textarea type="text" className="form-control" id="description" aria-describedby="Description input field" minLength="10" maxLength="300" rows="5" placeholder="Explain precisely what your need is here...." required />
+                  <textarea type="text" className="form-control" id="description" aria-describedby="Description input field" minLength="10" maxLength="300" rows="5" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Explain precisely what your need is here...." required />
                 </div>
               </div>
               <div className="d-flex flex-column flex-md-row justify-content-md-center mt-4">
-                <button type="submit" className="btn button-success button-modal me-0 me-md-2 mb-3 mb-md-0 p-1">Update profile</button>
+                <button type="submit" className="btn button-success button-modal me-0 me-md-2 mb-3 mb-md-0 p-1">Create request</button>
                 <button type="reset" className="btn button-warning button-modal ms-0 ms-md-2 p-1" onClick={() => closeNewRequestModal()}>Back</button>
               </div>
             </form>
