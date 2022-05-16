1d439137f03d8b3cee078fd7ea291517a23f5cc5
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-16 11:41:40 +0800

    edited delete request function

--- src/components/DropdownMenuOptionsRequestCard.jsx
+++ src/components/DropdownMenuOptionsRequestCard.jsx
@@ -1,15 +1,59 @@
 // CONFIG IMPORTS
-import React from 'react';
+import React, { useContext } from 'react';
+
+// CONTEXT IMPORTS
+import FlashContext from './Context/FlashContext';
+
+// DATA IMPORTS
+import baseURL from '../data/BaseURL';
 
 const DropdownMenuOptionsRequestCard = ({request, setOpenEditModal, setMarkRequestAsFulfilled}) => {
+  const { setFlash } = useContext(FlashContext);
+  const { id } = request;
   const markAsFulfilled = () => {
     alert("marking request as fulfilled");
   }
 
   const deleteRequest = () => {
-    window.confirm("You are about to delete this request. \n\nAre you sure?");
+    if (window.confirm("You are about to delete this request. \n\nAre you sure?")) {
+      const url = `${baseURL}/requests/${id}`;
+      const token = localStorage.getItem('jwt_token');
+
+      fetch(url, {
+        method: "DELETE",
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token,
+        },
+      })
+      .then(response => response.json())
+      .then(response => {
+        if (response.message) {
+          setFlash({
+            type: 'success',
+            message: response.message,
+            display: true,
+          });
+        } else {
+          setFlash({
+            type: 'danger',
+            message: response.error,
+            display: true,
+          })
+        }
+      })
+      .catch(error => {
+        setFlash({
+          type: 'danger',
+          message: error,
+          display: true,
+        })
+      })
+    }
   }
-  
+
   return (
     <div className="dropdown-menu-options-request-card ms-2">
       <ul className = "list-unstyled m-0 p-0">
