50412a6418d2d9799dc5dd96e8612c0477b85e57
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-17 14:22:33 +0800

    aded tabs to userRequests page

--- src/pages/UserRequests.jsx
+++ src/pages/UserRequests.jsx
@@ -16,6 +16,7 @@ const UserRequests = () => {
   const { setFlash } = useContext(FlashContext);
 
   const [currentUserRequests, setCurrentUserRequests] = useState('');
+  const [filteredCurrentUserRequests, setFilteredCurrentUserRequests] = useState('');
   const [currentUserRequest, setCurrentUserRequest] = useState('');
 
   const openNewRequestModal = (e) => {
@@ -221,10 +222,26 @@ const UserRequests = () => {
     })
   }
 
+
+  const filterRequests = (keyword) => {
+    if(keyword == '') {
+      setFilteredCurrentUserRequests(currentUserRequests)
+    } else {
+      let requests = currentUserRequests.filter((request) => request.status == keyword )
+      setFilteredCurrentUserRequests(requests)
+    }
+  };
+
   closeMenus();
   
   useEffect(() => {
     getCurrentUserRequests();
+  }, [currentUserRequests]);
+
+  useEffect(() => {
+    if (filteredCurrentUserRequests == '') {
+      filterRequests('')
+    }
   }, [currentUserRequests]);
 
   useEffect(() => {
@@ -240,7 +257,7 @@ const UserRequests = () => {
       <ShowRequestModal request={currentUserRequest} setOpenEditModal={openEditRequestModal} setMarkRequestAsFulfilled={markRequestAsFulfilled} setRepublishRequest={republishRequest} />
       <EditRequestModal request={currentUserRequest} />
       <div className="user-requests">
-        <div className="container d-flex justify-content-center mx-0 w-100">
+        <div className="d-flex justify-content-center mx-0 w-100">
           <div className="d-flex flex-column align-items-center my-3 py-3 w-100">
             <h1 className="text-primary text-center fw-bold pb-3 pb-md-4">My requests</h1>
             <button className="btn button-w150 button-outline-primary" id="signup-btn" onClick={(e) => openNewRequestModal()}>Request help</button>
@@ -252,21 +269,29 @@ const UserRequests = () => {
                 <span className="me-2" id="service"></span><p className="h5 m-0">Punctual service</p>
               </div>
             </div>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 d-flex justify-content-center w-100">
-              { currentUserRequests &&
-                currentUserRequests.map((request) => {
-                  return (
-                    <RequestCard 
-                      request={request} 
-                      setOpenShowModal={openShowRequestModal} 
-                      setOpenEditModal={openEditRequestModal}
-                      setMarkRequestAsFulfilled={markRequestAsFulfilled}
-                      setRepublishRequest={republishRequest}
-                      key={request.id} 
-                    />
-                  )
-                })
-              }
+            <div className="tabs-area d-flex flex-column align-items-center w-100 pb-3">
+              <ul className="d-flex justify-content-start w-100 p-0">
+                <li className="tab list-unstyled me-4 h4 text-center pointer" onClick={(e) => filterRequests('')}>All</li>
+                <li className="tab list-unstyled mx-4 h4 text-center pointer" onClick={(e) => filterRequests('in progress')}>In progress</li>
+                <li className="tab list-unstyled mx-4 h4 text-center pointer" onClick={(e) => filterRequests('expired')}>Expired</li>
+                <li className="tab list-unstyled me-4 h4 text-center pointer" onClick={(e) => filterRequests('fulfilled')}>Fulfilled</li>
+              </ul>
+              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 d-flex justify-content-center w-100">
+                { filteredCurrentUserRequests &&
+                  filteredCurrentUserRequests.map((request) => {
+                    return (
+                      <RequestCard 
+                        request={request} 
+                        setOpenShowModal={openShowRequestModal} 
+                        setOpenEditModal={openEditRequestModal}
+                        setMarkRequestAsFulfilled={markRequestAsFulfilled}
+                        setRepublishRequest={republishRequest}
+                        key={request.id} 
+                      />
+                    )
+                  })
+                }
+              </div>
             </div>
           </div>
         </div>
