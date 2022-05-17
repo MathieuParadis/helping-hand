c21571c7709380bd18bbe7cc460d408e3f9e5ea1
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-17 16:32:43 +0800

    worked on tge styling of the tabs

--- src/pages/UserRequests.jsx
+++ src/pages/UserRequests.jsx
@@ -270,10 +270,10 @@ const UserRequests = () => {
             </div>
             <div className="tabs-area d-flex flex-column align-items-center w-100 pb-3">
               <ul className="row row-cols-md-2 row-cols-md-4 p-0 w-100">
-                <li className="list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3" onClick={(e) => filterRequests('')}>All</li>
-                <li className="list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3" onClick={(e) => filterRequests('in progress')}>In progress</li>
-                <li className="list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3" onClick={(e) => filterRequests('expired')}>Expired</li>
-                <li className="list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3" onClick={(e) => filterRequests('fulfilled')}>Fulfilled</li>
+                <li className="tab list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3 border-bottom-prim" onClick={(e) => filterRequests('')}>All</li>
+                <li className="tab list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3 border-bottom-prim border-left-prim" onClick={(e) => filterRequests('in progress')}>In progress</li>
+                <li className="tab list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3 border-bottom-prim border-left-md-prim" onClick={(e) => filterRequests('expired')}>Expired</li>
+                <li className="tab list-unstyled my-0 py-2 h4 text-center pointer col-6 col-md-3 border-bottom-prim border-left-prim" onClick={(e) => filterRequests('fulfilled')}>Fulfilled</li>
               </ul>
               <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 d-flex justify-content-center pt-2 w-100">
                 { filteredCurrentUserRequests &&
