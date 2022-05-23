451741e4024a5b377377ec5919d0c460cb009f10
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-23 14:37:46 +0800

    added action cable provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,25 @@
+// CONFIG IMPORT
 import React from 'react';
+
+// REACT DOM IMPORT
 import ReactDOM from "react-dom/client";
+
+// ACTION CABLE IMPORT
+import { ActionCableProvider } from 'react-actioncable-provider';
+
+// APP IMPORT
 import App from './App';
+
+// STYLES IMPORT
 import './styles/index.scss';
+
+// URL IMPORT
+import { API_WS_ROOT  } from './constants/index';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ActionCableProvider url={API_WS_ROOT}>
     <App />
-  </React.StrictMode>
+  </ActionCableProvider>
 );
