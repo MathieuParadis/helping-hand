874e8cfea0087eae615d0bc681ddab712c558774
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-05-26 12:09:57 +0800

    added date to ChatMessage

--- src/components/ActionCable/ChatMessage.jsx
+++ src/components/ActionCable/ChatMessage.jsx
@@ -4,13 +4,17 @@ import React, { useEffect, useContext } from 'react';
 // MOMENT INPORTS
 import Moment from 'react-moment';
 import 'moment-timezone';
+import moment from 'moment';
 
 // CONTEXT IMPORTS
 import UserContext from '../Context/UserContext';
 
 const ChatMessage = ({message, previousMessage}) => {
   const { user } = useContext(UserContext);
-  const UserTz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const userTZ = moment.tz.guess();
+  const previousMessageDate = moment(previousMessage.created_at).tz(userTZ).format("MMM Do YY");
+  const messageDate = moment(message.created_at).tz(userTZ).format("MMM Do YY");
 
   const stylingMessage = () => {
     const chatMessage = document.querySelector("#message-" + message.id);
@@ -28,8 +32,6 @@ const ChatMessage = ({message, previousMessage}) => {
     } 
   }
 
-  console.log(previousMessage)
-
   useEffect(() => {
     stylingMessage();
   }, []);
@@ -37,12 +39,16 @@ const ChatMessage = ({message, previousMessage}) => {
   return (
     <div className="chat-message d-flex flex-column mb-2" id={`message-${message.id}`}>
       {
+        (previousMessage === '' || previousMessageDate !== messageDate) &&
+        <Moment tz={userTZ} format="LL" className="message-date h5 pb-2 text-center w-100">{message.created_at}</Moment>
+      }
+      {
         (previousMessage === '' || previousMessage.user.id !== message.user.id) &&
         <p className="m-0 pb-2">{message.user.first_name} says:</p>
       }
       <div className="m-0 px-4 py-2 message-body text-white border-radius-8">
         <p className="m-0">{message.content}</p>
-        <Moment tz={UserTz} format="HH:mm" className="message-time">{message.created_at}</Moment>
+        <Moment tz={userTZ} format="HH:mm" className="message-time">{message.created_at}</Moment>
       </div>
     </div>
   );
