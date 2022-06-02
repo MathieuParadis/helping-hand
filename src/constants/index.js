cdd88b53c7ed39836e0ec026cbe1ca1788f42155
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-06-02 21:04:31 +0800

    edited ws url

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,6 @@
 const API_ROOT = 'https://helpping-hand-back.herokuapp.com';
 
 // const API_WS_ROOT = 'ws://localhost:3000/cable';
-const API_WS_ROOT = 'ws://helpping-hand-back.herokuapp.com/cable';
+const API_WS_ROOT = 'wss://helpping-hand-back.herokuapp.com/cable';
 
 export { API_ROOT, API_WS_ROOT };
